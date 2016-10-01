f0b9477d640d4fcfb89ff163cccf76aa2ac255b8
xgasior <o.gasior@hotmail.com>
2016-10-01 16:58:13 +0200

    Init

---
+++ 448273/app/src/main/java/cz/muni/fi/pv256/movio2/a448273/Secret/Keys.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pv256.movio2.a448273.Secret;
+
+/**
+ * Created by gasior on 01.10.2016.
+ */
+
+public class Keys {
+    public final static String  API_KEY__MOVIE_DB = "1c77b19f08b023b7320e53e2717d92cd";
+}
