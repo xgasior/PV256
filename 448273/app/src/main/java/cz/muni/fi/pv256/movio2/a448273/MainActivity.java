a163585ba0ac4b76029d484e139bf59a9d3b6d25
xgasior <o.gasior@hotmail.com>
2016-10-01 21:11:29 +0200

    ukol2

--- 448273/app/src/main/java/cz/muni/fi/pv256/movio2/a448273/MainActivity.java
+++ 448273/app/src/main/java/cz/muni/fi/pv256/movio2/a448273/MainActivity.java
@@ -1,13 +1,41 @@
 package cz.muni.fi.pv256.movio2.a448273;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button mThemeSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean isPunk = prefs.getBoolean("punk", false);
+        if(isPunk) {
+            setTheme(R.style.MaterialStyle);
+        } else {
+            setTheme(R.style.MaterialStyle_Punk);
+        }
+        prefs.edit().putBoolean("punk", !isPunk).commit();
+
         setContentView(R.layout.activity_main);
+
+
+        setTheme(R.style.MaterialStyle_Punk);
+        mThemeSwitch = (Button)findViewById(R.id.theme_switch);
+        mThemeSwitch.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
